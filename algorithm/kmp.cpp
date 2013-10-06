77462ebaf37528c685c23df775aad8fd5d69b08d
albon arith <albon.arith@gmail.com>
2013-10-06 19:25:39 +0800

    KMP算法
    next[j]的含义是，当p[j]!=text[i]时，应该拿p[next[j]]再跟text[i]比较一下。
    如果p[0]!=text[i]，那就i++，再跟p[0]比较吧。

---
+++ algorithm/kmp.cpp
@@ -0,0 +1,62 @@
+/**
+ * kmp算法的重点是next数组的计算
+ */
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void compute_prefix(int *next, char *p)
+{// 计算next数组
+    int i, n, k;
+    n = strlen(p);
+    next[1] = next[0] = 0;
+    k = 0;
+    for(i=2; i<n; ++i)
+    {
+        for(;k!=0 && p[k]!=p[i-1]; k=next[k]);
+        if(p[k] == p[i-1])
+            k++;
+        next[i] = k;
+    }
+}
+
+void kmp_match(char* text, char *p, int *next)
+{
+    int m, n, s, q;
+    m = strlen(p);
+    n = strlen(text);
+    q = s = 0;
+
+    while(s < n)
+    {// p[q]!=text[s]时，令q=next[q]再比较
+        for(q=next[q]; q<m && s<n && p[q]==text[s]; q++, s++);
+        if(q == 0)
+        {
+            s++;
+        }
+        else if(q==m)
+        {
+            q = 0;
+            cout<<"pattern occurs with shift"<< s-m<<endl;
+        }
+
+    }
+}
+
+int main()
+{
+    int next[101], n;
+    char *p = "abacc";
+    char *text = "ababccccababc";
+
+    compute_prefix(next, p);
+    cout<<"prefix: ";
+    for(int i=0; i<strlen(p); ++i)
+    {
+        cout<<next[i]<<" ";
+    }
+    cout<<endl;
+    kmp_match(text, p, next);
+    return 0;
+}
